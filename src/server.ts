dd88db49ee713632f1c7e9703bfb2e1daffdc109
Marcin Kędziora <marcinkedziora@icloud.com>
2024-10-17 14:38:09 +0200

    Add fetch market data endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express, { Express, Request, Response } from "express";
-import { initializeBinanceConnector } from "./services/data.service";
+import {
+  fetchMarketDataForSymbol,
+  initializeBinanceConnector,
+} from "./services/data.service";
 
 void main();
 
@@ -7,13 +10,35 @@ async function main() {
   const app: Express = express();
   const port = 3000;
 
-  await initializeBinanceConnector();
+  const connector = await initializeBinanceConnector();
 
   app.get("/", (req: Request, res: Response) => {
     res.send("hello");
+  });
+
+  // GET /data?symbol=""&range=""
+  app.get("/data", async (req: Request, res: Response) => {
+    const symbol = req.query.symbol;
+    const range = req.query.range;
+
+    if (!symbol || !range) {
+      res.status(400).send("Invalid parameters");
+      return
+    }
+
+    const result = await fetchMarketDataForSymbol(connector)({
+      symbol: String(symbol),
+      timeRange: String(range),
+    });
+    res.send(result);
   });
 
   app.listen(port, () => {
     console.log(`Server is running on localhost:${port}`);
   });
 }
+
+// TODO:
+// 1. Error handling
+// 2. Validate input
+// 3. implement auto restart on file save for saving dev time
--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,6 +1,22 @@
 import { Spot } from "@binance/connector-typescript";
 
-export { initializeBinanceConnector };
+export {
+  initializeBinanceConnector,
+  fetchMarketDataForSymbol,
+  FetchMarketDataInput,
+  FetchMarketDataOutput,
+};
+
+type FetchMarketDataInput = {
+  symbol: string;
+  timeRange: string;
+};
+
+type FetchMarketDataOutput = {
+  symbol: string;
+  priceChange: string;
+  priceChangePercent: string;
+};
 
 const initializeBinanceConnector = async (): Promise<Spot> => {
   const client = new Spot("", "", {
@@ -9,8 +25,36 @@ const initializeBinanceConnector = async (): Promise<Spot> => {
 
   try {
     await client.testConnectivity();
-    return client
+    return client;
   } catch (error) {
     throw new Error(`Failed to connect to Binance API, reason ${error}`);
   }
 };
+
+const fetchMarketDataForSymbol =
+  (connector: Spot) =>
+  async (input: FetchMarketDataInput): Promise<FetchMarketDataOutput> => {
+    try {
+      const result = await connector.rollingWindowPriceChangeStatistics({
+        symbol: input.symbol,
+        windowSize: input.timeRange,
+      });
+      if (
+        "symbol" in result &&
+        "priceChange" in result &&
+        "priceChangePercent" in result
+      )
+        return {
+          symbol: result.symbol,
+          priceChange: Number(result.priceChange).toString(), //prevent trailing zeros
+          priceChangePercent: formatPercent(result.priceChangePercent),
+        };
+
+      throw new Error("Invalid data output");
+    } catch (error) {
+      throw new Error(`Failed to fetch data from Binance API, ${error}`);
+    }
+  };
+
+const formatPercent = (percent: string): string =>
+  String(Number(percent) * 100) + "%";
